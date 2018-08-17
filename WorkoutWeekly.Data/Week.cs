f458361c842c638b8d9a5c1563053fcbe7556e6d
Merideth Bush <meridethbush@gmail.com>
2018-08-17 11:01:27 -0400

    new classes

---
+++ WorkoutWeekly.Data/Week.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutWeekly.Data
+{
+    public enum DaysOfWeek
+    {
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+    public class Week
+    {
+        public DaysOfWeek Day { get; set; }
+    }
+}
---
+++ WorkoutWeekly.Data/WorkoutType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutWeekly.Data
+{
+    public class WorkoutType
+    {
+        public string Type { get; set; }
+    }
+}
